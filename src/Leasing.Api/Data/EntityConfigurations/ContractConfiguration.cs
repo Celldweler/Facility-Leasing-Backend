b888aa4d147fc5b291b41311b5f3d2df423262e5
Fedir Buzhylov <fedir.buzhylov@gmail.com>
2024-12-05 16:45:59 +0300

    Implement Domain model and Data access with Entity Framework Core and MS SqlServer database.

---
+++ src/Leasing.Api/Data/EntityConfigurations/ContractConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Leasing.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Leasing.Api.Data.EntityConfigurations;
+
+public class ContractConfiguration : IEntityTypeConfiguration<Contract>
+{
+    public void Configure(EntityTypeBuilder<Contract> builder)
+    {
+        builder.HasKey(x => new { x.EquipmentCode, x.FacilityCode });
+        
+        builder.Property(x => x.EquipmentQuantity).IsRequired();
+    }
+}
---
+++ src/Leasing.Api/Domain/ProductionFacility.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Leasing.Api.Common;
+
+namespace Leasing.Api.Domain;
+
+public class ProductionFacility : IEntity
+{
+    public int Code { get; set; }
+    
+    public string Name { get; set; }
+
+    [Range(Constants.MinProductionFacilityArea, float.MaxValue, ErrorMessage = "Area must be greater than 1000")]
+    public float Area { get; set; }
+}
